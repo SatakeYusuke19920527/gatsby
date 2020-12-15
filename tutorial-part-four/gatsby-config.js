1df05420bcc2f055b8b1c98cc1ae8e8ed69b9fab
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-15 09:10:13 +0900

    gatsby tutorial part6 fin

--- tutorial-part-four/gatsby-config.js
+++ tutorial-part-four/gatsby-config.js
@@ -1,10 +1,19 @@
 module.exports = {
   siteMetadata: {
     title: `Title from siteMetadata`,
+    description: `description`,
     message: `Message from siteMetadata`,
   },
   plugins: [
     `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- tutorial-part-four/src/pages/about.js
+++ tutorial-part-four/src/pages/about.js
@@ -1,15 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql,useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function About() {
   const data = useStaticQuery(
     graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            message
+        query {
+          markdownRemark{
+            frontmatter{
+              title,
+              date
           }
         }
       }
@@ -18,13 +18,13 @@ export default function About() {
   console.log(data, "graphql 確認")
   return (
     <Layout>
-      <h1>{data.site.siteMetadata.title}</h1>
+      <h1>Title</h1>
       <p>
         We're the only site running on your computer dedicated to showing the
         best photos and videos of pandas eating lots of food.
       </p>
       <p>
-        {data.site.siteMetadata.message}
+        message
       </p>
     </Layout>
   )
